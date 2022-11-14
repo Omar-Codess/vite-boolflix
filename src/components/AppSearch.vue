62ac3d8fd8178b79c035e8bc5af8090b01bff5aa
Omar-Codess <omartaki8@gmail.com>
2022-11-14 17:35:09 +0100

    creato componente AppSearch

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <div class="search-bar">
+        <input type="text">
+        <button>Cerca</button>
+    </div>
+</template>
+
+<style>
+.flex{
+    display: flex;
+}
+
+.search-bar{
+    margin: 0 auto;
+}
+</style>
