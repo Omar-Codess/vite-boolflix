fbe7b73cd6d1c12df77e5e6772b0473b68a794b5
Omar-Codess <omartaki8@gmail.com>
2022-11-15 12:46:07 +0100

    installato axios da terminale

--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import { reactive } from `vue`
 
 export const store = reactive ({
     moviesList: [],
-    apiURL: "https://api.themoviedb.org/3/movie/550?api_key=4d580452c3bd253a25f34d068a8cd030&query=ciao"
+    apiURL: "https://api.themoviedb.org/3/movie/550?api_key=4d580452c3bd253a25f34d068a8cd030&query=ciao";
+    
 })
