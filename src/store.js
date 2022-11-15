f4424c4e79a50eb190f58fd9b37e19ca453a618c
Omar-Codess <omartaki8@gmail.com>
2022-11-15 13:01:22 +0100

    aggiunto apiKey a store.js

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { reactive } from `vue`
 
 export const store = reactive ({
     moviesList: [],
-    apiURL: "https://api.themoviedb.org/3/movie/550?api_key=4d580452c3bd253a25f34d068a8cd030&query=ciao";
-    
+    seriesList: [],
+    apiURL: "https://api.themoviedb.org/3/search",
+    apiKey:  "4d580452c3bd253a25f34d068a8cd030",
+    searchKey: "",
 })
